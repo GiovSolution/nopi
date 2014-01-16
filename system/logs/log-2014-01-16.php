20382e85852ee14849a9d9f4d8a7a4e8f38d04a7
hypolution <freddy@giovsolution.com>
2014-01-16 17:11:06 +0700

    system logs

--- system/logs/log-2014-01-16.php
+++ system/logs/log-2014-01-16.php
@@ -4,3 +4,9 @@ ERROR - 2014-01-16 13:19:59 --> Query error:
 ERROR - 2014-01-16 13:25:05 --> Query error: 
 ERROR - 2014-01-16 13:41:12 --> Severity: Notice  --> Undefined index:  jproduk_nobukti_pajak /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_jual_produk.php 403
 ERROR - 2014-01-16 15:34:30 --> Query error: 
+ERROR - 2014-01-16 16:50:37 --> Severity: Notice  --> Undefined index:  fpiutang_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_lunas_piutang.php 143
+ERROR - 2014-01-16 16:50:37 --> Severity: Notice  --> Undefined index:  selected_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_lunas_piutang.php 146
+ERROR - 2014-01-16 16:50:39 --> Severity: Notice  --> Undefined index:  fpiutang_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_lunas_piutang.php 143
+ERROR - 2014-01-16 16:50:39 --> Severity: Notice  --> Undefined index:  selected_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_lunas_piutang.php 146
+ERROR - 2014-01-16 16:50:44 --> Severity: Notice  --> Undefined index:  fpiutang_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_lunas_piutang.php 143
+ERROR - 2014-01-16 16:50:44 --> Severity: Notice  --> Undefined index:  selected_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_lunas_piutang.php 146
