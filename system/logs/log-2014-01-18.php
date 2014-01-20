4da05cd2813872514efc532d465abc20efe5531e
hypolution <freddy@giovsolution.com>
2014-01-20 13:54:37 +0700

    Test

---
+++ system/logs/log-2014-01-18.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-01-18 14:33:07 --> Severity: Notice  --> Undefined index:  selected_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_retur_jual_produk.php 45
+ERROR - 2014-01-18 14:33:31 --> Severity: Notice  --> Undefined index:  satuan /Applications/MAMP/htdocs/nopi/system/application/controllers/c_hpp.php 33
+ERROR - 2014-01-18 14:33:41 --> Severity: Notice  --> Undefined index:  query /Applications/MAMP/htdocs/nopi/system/application/controllers/c_kartu_stok.php 53
+ERROR - 2014-01-18 14:33:41 --> Severity: Notice  --> Undefined index:  start /Applications/MAMP/htdocs/nopi/system/application/controllers/c_kartu_stok.php 54
+ERROR - 2014-01-18 14:33:41 --> Severity: Notice  --> Undefined index:  limit /Applications/MAMP/htdocs/nopi/system/application/controllers/c_kartu_stok.php 55
+ERROR - 2014-01-18 14:33:41 --> Severity: Notice  --> Undefined index:  query /Applications/MAMP/htdocs/nopi/system/application/controllers/c_kartu_stok.php 96
+ERROR - 2014-01-18 14:33:41 --> Severity: Notice  --> Undefined index:  start /Applications/MAMP/htdocs/nopi/system/application/controllers/c_kartu_stok.php 97
+ERROR - 2014-01-18 14:33:41 --> Severity: Notice  --> Undefined index:  limit /Applications/MAMP/htdocs/nopi/system/application/controllers/c_kartu_stok.php 98
+ERROR - 2014-01-18 15:02:11 --> Severity: Notice  --> Undefined index:  selected_id /Applications/MAMP/htdocs/nopi/system/application/controllers/c_master_retur_jual_produk.php 45
